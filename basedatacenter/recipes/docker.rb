df6b8f2fafb48cfd916cdb4d1f391ed2a7a14961
ashwinrayaprolu1984 <ashwin.rayaprolu@gmail.com>
2016-10-02 23:31:07 -0700

    Added docker-compose.yaml and Copied docker env files to each rack in chef recipe

--- basedatacenter/recipes/docker.rb
+++ basedatacenter/recipes/docker.rb
@@ -7,3 +7,12 @@ execute "Install Docker Environment" do
   command "sh Install.sh"
   not_if 'service --status-all | grep "docker"' # Run the above command if docker is not installed
 end
+
+
+remote_directory '/home/docker/multinode/' do
+  source 'docker/multinode'
+  owner 'docker'
+  group 'docker'
+  mode '0755'
+  action [:delete , :create]
+end
